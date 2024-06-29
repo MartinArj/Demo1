af5242ebe3521f11efeec0f90eada52ae3c2e0df
Rahamath22 <rahamath22104@gmail.com>
2024-06-29 15:41:09 +0530

    Some Extra pages

--- StudentApp/StudentApp/AddStudent.xaml.cs
+++ StudentApp/StudentApp/AddStudent.xaml.cs
@@ -19,9 +19,43 @@ namespace StudentApp
     /// </summary>
     public partial class AddStudent : Page
     {
+        StudDetails student;
+        public List <int> class_No{get;set;}
         public AddStudent()
         {
+            
             InitializeComponent();
+            class_No = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            studCls.ItemsSource = class_No;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AddStudent_panel.Visibility = Visibility.Collapsed;
+            Stud_Address.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            AddStudent_panel.Visibility = Visibility.Visible;
+            Stud_Address.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AddStudent());
+           
+        }
+
+        public void insertstudent()
+        {
+            string StudentName = studName.Text;
+            string StudentSection = studSec.Text;
+            string StudentYear = studYear.Text;
+            string StudentDOB = studDob.Text;
+            string StudentBldGrp = studBldgrp.Text;
+            int StudentClass = (int)studCls.SelectedItem;
+            student = new StudDetails(StudentClass, StudentSection, StudentYear, StudentName, StudentDOB, StudentBldGrp);
         }
     }
 }
